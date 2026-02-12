7d4ee0f40ab5486d64396d9fd6e48565a286324e
Felix708 <samuelfs561@gmail.com>
2026-02-12 08:27:42 +0700

    added new component for FAQs and "supported payment"

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import CarouselComponent from "./components/CarouselComponent"
 import CardWrapperComponent from "./components/CardWrapperComponent"
 import { Spinner } from "flowbite-react";
 import ReadMoreWrapperComponent from "./components/ReadMoreWrapperComponent"
+import PaymentComponent from "./components/PaymentComponent"
+import AccordionComponent from "./components/AccordionComponent"
 
 
 export default function App() {
@@ -13,6 +15,40 @@ export default function App() {
   ]);
   const [platformType, setPlatformType] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [question, setQuestion] = useState([
+    {
+      id: 1,
+      title: "What makes Epstein's Goodies better?",
+      content: "Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more."
+    },
+    {
+      id: 2,
+      title: "Does the items in Epstein's Goodies legit?",
+      content: "Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file."
+    },
+    {
+      id: 3,
+      title: "How to send people 6 feet under without alerting the cops?",
+      content: "The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages."
+    }
+  ]);
+  const [payment, setPayment] = useState([
+    {
+      id: 1,
+      title: "Paypal",
+      image: "https://www.pngall.com/wp-content/uploads/5/PayPal-Logo-PNG-Free-Image.png",
+    },
+    {
+      id: 2,
+      title: "Credit Card",
+      image: "https://tse4.mm.bing.net/th/id/OIP.kDaG0rL2cfRuyMlfHhLfqAHaHa?pid=Api&P=0&h=180",
+    },
+    {
+      id: 3,
+      title: "Apple Pay",
+      image: "https://tse1.mm.bing.net/th/id/OIP.tJKOUZzlAnV60dSnP7OXfgHaE8?pid=Api&P=0&h=180",
+    },
+  ]);
 
   async function getStoresData() {
     const url = "https://www.cheapshark.com/api/1.0/stores";
@@ -48,6 +84,8 @@ export default function App() {
             <CarouselComponent CarouselImages={carouselImg} />
             <CardWrapperComponent type={platformType}></CardWrapperComponent>
             <ReadMoreWrapperComponent />
+            <PaymentComponent methods={payment} />
+            <AccordionComponent question={question} />
           </div>
         )
       }
